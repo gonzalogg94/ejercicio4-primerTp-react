2c2735f1434f15b050a170fb716bc1f09121faf9
Gonzalo Gallardo <gonzalo94.jobs@gmail.com>
2022-09-25 18:46:16 -0300

    agregamos el maquetado a los componentes formulario lista e item tarea

---
+++ src/components/ListaTarea.jsx
@@ -0,0 +1,12 @@
+import ItemTarea from "./ItemTarea";
+import {ListGroup} from 'react-bootstrap';
+
+const ListaTarea = () => {
+    return(
+        <ListGroup> 
+<ItemTarea></ItemTarea>
+      </ListGroup>
+    );
+};
+
+export default ListaTarea;
